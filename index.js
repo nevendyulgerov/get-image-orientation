297182d935ebd96ebbe4192ce400958461791d74
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-07-30 17:22:17 +0300

    Add index

---
+++ index.js
@@ -0,0 +1,36 @@
+
+/**
+ * @description To int
+ * @param value
+ * @returns {number}
+ */
+const toInt = value => parseInt(value, 10);
+
+/**
+ * @description Get image aspect type
+ * @param width
+ * @param height
+ * @param maxIncrease
+ * @returns object
+ */
+const getImageOrientation = (width, height, maxIncrease = 15) => {
+  const isSquare = width === height;
+  const isLandscape = !isSquare && width > height;
+  const isPortrait = !isSquare && !isLandscape;
+  const widthIncrease = width > height ? toInt(((width - height) * 100) / width) : 0;
+  const heightIncrease = height > width ? toInt(((height - width) * 100) / height) : 0;
+  const isSquareLikeLandscape = isLandscape && widthIncrease < maxIncrease;
+  const isSquareLikePortrait = isPortrait && heightIncrease < maxIncrease;
+
+  return {
+    isLandscape,
+    isPortrait,
+    isSquare,
+    isSquareLikeLandscape,
+    isSquareLikePortrait,
+    widthIncrease,
+    heightIncrease
+  };
+};
+
+module.exports = getImageOrientation;
